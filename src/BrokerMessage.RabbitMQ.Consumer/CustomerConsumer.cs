9a8e28eb7a39551a8b22bc359592624add753d2a
ranciaro <renan.ranciaro@gmail.com>
2020-05-23 21:37:01 -0300

    first commit

---
+++ src/BrokerMessage.RabbitMQ.Consumer/CustomerConsumer.cs
@@ -0,0 +1,27 @@
+﻿using BrokerMessage.RabbitMQ.Core;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BrokerMessage.RabbitMQ.Consumer
+{
+    public class CustomerConsumer : IHostedService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            new Producer().Receive();
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+        private void DoWork(object state)
+        {
+            new Producer().Receive();
+            Console.WriteLine("executei ");
+        }
+    }
+}
---
+++ src/BrokerMessage.RabbitMQ.Producer/Controllers/MessageBrokerController.cs
@@ -0,0 +1,18 @@
+﻿using BrokerMessage.RabbitMQ.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BrokerMessage.RabbitMQ.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageBrokerController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult SendMessage()
+        {            
+            new Producer().Send();
+            //new Producer().Receive();
+            return Ok();
+        }
+    }
+}
